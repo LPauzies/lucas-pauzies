e41d91eb402b58b642a2ace685e3b892c0ea704f
Lucas Pauzies <lucas.pauzies@hotmail.fr>
2022-04-13 17:24:24 +0200

    Move to Svelte (#4)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,22 @@
-'use strict';
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true,
-    },
+    ecmaVersion: 2019,
+    sourceType: "module",
   },
-  plugins: ['ember'],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:prettier/recommended',
-  ],
   env: {
+    es6: true,
     browser: true,
+    jest: true,
+    node: true,
   },
-  rules: {},
+  extends: ["eslint:recommended", "plugin:import/recommended"],
+  plugins: ["svelte3"],
   overrides: [
-    // node files
     {
-      files: [
-        './.eslintrc.js',
-        './.prettierrc.js',
-        './.template-lintrc.js',
-        './ember-cli-build.js',
-        './testem.js',
-        './blueprints/*/index.js',
-        './config/**/*.js',
-        './lib/*/index.js',
-        './server/**/*.js',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-      env: {
-        browser: false,
-        node: true,
-      },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
-      rules: {
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off',
-      },
-    },
-    {
-      // test files
-      files: ['tests/**/*-test.{js,ts}'],
-      extends: ['plugin:qunit/recommended'],
+      files: ["**/*.svelte"],
+      processor: "svelte3/svelte3",
     },
   ],
+  rules: {},
+  settings: {},
 };
