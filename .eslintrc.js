148ca237a1db00416956dd86b2e9b8ab566b5601
Cair0n <lucas.pauzies@hotmail.fr>
2022-04-14 18:40:24 +0200

    End of the setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,35 @@
+// .eslintrc.js
+
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
   parserOptions: {
-    ecmaVersion: 2019,
+    ecmaVersion: 2020,
     sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+    extraFileExtensions: [".svelte"],
   },
   env: {
     es6: true,
     browser: true,
-    jest: true,
-    node: true,
   },
-  extends: ["eslint:recommended", "plugin:import/recommended"],
-  plugins: ["svelte3"],
   overrides: [
     {
-      files: ["**/*.svelte"],
+      files: ["*.svelte"],
       processor: "svelte3/svelte3",
     },
   ],
-  rules: {},
-  settings: {},
+  settings: {
+    "svelte3/typescript": require("typescript"),
+    // ignore style tags in Svelte because of Tailwind CSS
+    // See https://github.com/sveltejs/eslint-plugin-svelte3/issues/70
+    "svelte3/ignore-styles": () => true,
+  },
+  plugins: ["svelte3", "@typescript-eslint"],
+  ignorePatterns: ["node_modules"],
 };
