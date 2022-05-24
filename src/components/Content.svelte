f8710df59c871158a4e38a0f22533d917305bdb2
Cair0n <lucas.pauzies@hotmail.fr>
2022-05-24 16:19:00 +0200

    Navbar for Github actions

---
+++ src/components/Content.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import TitleContent from "./content/TitleContent.svelte";
+    import Jobs from "./content/Jobs.svelte";
+</script>
+
+<main>
+    <div class="repository-content px-3 mt-5">
+        <TitleContent/>
+        <div class="d-flex flex-column flex-md-row">
+            <Jobs/>
+        </div>
+    </div>
+</main>
+
+<style lang="scss">
+	@import "../style/colors";
+
+    .repository-content {
+        width: 100%;
+    }
+</style>
